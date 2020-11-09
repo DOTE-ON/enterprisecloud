e9b067710e8ebb2890756306b7ff28826f0c393b
Miyano <miyano.nakara@gmail.com>
2020-11-09 23:48:04 +0800

    first commit

---
+++ src/main/java/com/miyano/enterprisecloud/EnterpriseCloudApplication.java
@@ -0,0 +1,16 @@
+package com.miyano.enterprisecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Miyano
+ */
+@SpringBootApplication
+public class EnterpriseCloudApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (EnterpriseCloudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/miyano/enterprisecloud/ServletInitializer.java
@@ -0,0 +1,16 @@
+package com.miyano.enterprisecloud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Miyano
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+        return application.sources (EnterpriseCloudApplication.class);
+    }
+
+}
